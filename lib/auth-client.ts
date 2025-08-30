fd037dd82b40baab9e846a704aee0232f0d141ca
“jatin-j” <infinitywastaken80808080@gmail.com>
2025-08-30 18:44:52 +0530

    fix: prod fixes

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000/",
+  baseURL: `${process.env.BETTER_AUTH_URL}`,
 });
 const signInGoogle = async () => {
   const data = await authClient.signIn.social({
